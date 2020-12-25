c856c2c879799fb9e6756a4cdaf343a3a170375d
jasonp000 <jasonp000@gmail.com>
2020-12-25 09:08:50 -0500

    updates

--- 4-four-dirty-dishes/main.tf
+++ 4-four-dirty-dishes/main.tf
@@ -1,5 +1,6 @@
-#variable "access_key" {}
-#variable "secret_key" {}
+variable "access_key" {}
+variable "secret_key" {}
+variable "aws_session_token" {}
 variable "region" {
   default = "us-east-1"
 }
@@ -17,8 +18,9 @@ variable "ssh_port" {}
 data "aws_availability_zones" "all" {}
 
 provider "aws" {
-#  access_key = var.access_key
-#  secret_key = var.secret_key
+  access_key = var.access_key
+  secret_key = var.secret_key
+  token      = var.aws_session_token
   region     = var.region
 }
 
